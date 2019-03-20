9f5f620a9caab0ce922f4cab6202621b4cfcf8ef
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2019-03-20 08:21:26 +0100

    Add which_graphic method and facilities for adding photos of cards

--- traverse_create_new.cpp
+++ traverse_create_new.cpp
@@ -1,11 +1,38 @@
 #include "traverse_create_new.h"
+#include "game.h"
 
 #include <QDebug>
+
+extern Game *game;
 
 Traverse_Create_new::Traverse_Create_new(){
 
 }
 
-void Traverse_Create_new::Learn(int x){
-    qDebug() << "in learn" << x;
+void Traverse_Create_new::Learn(int value){
+
+    game->scene->clear();
+
+
+    add_button( value, 100, 100 );
+
 }
+
+void Traverse_Create_new::add_button(int value, int x_pos, int y_pos){
+
+    Graphic_others *picture = new Graphic_others();
+    picture->setPixmap(picture->setImageOthers(value));
+    picture->setPos(x_pos, y_pos);
+    game->scene->addItem(picture);
+
+}
+
+void Traverse_Create_new::which_graphic(int value){
+
+    // Chose the group of cards to learning
+    if(value == 0)      group_card = 0;
+    else if(value == 1) group_card = 13;
+    else if(value == 2) group_card = 26;
+    else if(value == 3) group_card = 39;
+}
+
--- traverse_create_new.h
+++ traverse_create_new.h
@@ -14,6 +14,15 @@ public:
     // Public Attributes
     void Learn(int x);
 
+
+private:
+    // Private Methods
+    void add_button(int value, int x_pos, int y_pos);
+    void which_graphic(int value);
+
+    // Pribate Attributes
+    QList<Graphic_others> list;
+    int group_card;
 };
 
 #endif // TRAVERSE_CREATE_H
